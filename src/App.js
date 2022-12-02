7f65a9339d5ebdb475526e872f7f0ad7bb3fc218
lssport <111240117+lssport@users.noreply.github.com>
2022-12-02 21:31:08 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.js";
-export default function (App) {
-  return (
+class App extends React.Component {
+ *// constructor() {
+    //super();//
+//}*//
+  //render() {*//
+   // return {//*
     <div className='container py-5'>
     <div className='row'>
     <div className='col-12 text-center'>
@@ -11,6 +15,8 @@ export default function (App) {
     <hr className='w-75 mx-auto'/>
     <h2 className='fw-bold'>Rp.1.500.000,-</h2>
     <span className='title-m'>sisa uang kamu tersisa 75% lagi</span>
+    </div>
+    </div>
     <div className='row'>
     <div className='col-'>
     <div className='card-wrapper p-4'>
@@ -24,28 +30,21 @@ export default function (App) {
     <div className='col-12 d-flex justify-content-between align-items-center'>
     <h4>ringkasan transaksi</h4>
     <div className='wrapper-button'>
-      <button className='button btn-ungu'>pengeluaran<i class="bi bi-plus-circle-fil"></i></button>
-      <button className='button btn-pink'>pengeluaran<i class="bi bi-dash-circle-fil"></i></button>
+      <button className='button btn-ungu px-3 py-2 me-2'>pengeluaran<i class="bi bi-plus-circle-fil"></i></button>
+      <button className='button btn-pink px-3 py-2'>pengeluaran<i class="bi bi-dash-circle-fil"></i></button>
     <div className='row mt-5'>
       <div className='col-12 d-flex 
     justify-content-between align-items-center'>
     <div className='d-flex align-item-center'>
 <div className='icon-wrapper'>
   <i class="bi bi-wallet2"></i>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-  </div>
-
-  </div>
-</div>
-</div>
-</div>
+    </div>
+    <div className='transaction ms-2 d-flex flex-column'>
+    <h6>Menerima Gaji</h6>
+    <span className='title-sm'>1 july 2022</span>
     </div>
     </div>
-    </div>
-  );
+    <h5 className='money'>Rp.1.000.000,-</h5>
+   }
 }
+export default (App};
